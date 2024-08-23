a5be984e604a8a76b7f1166bce4d21ebe723f624
geofannyher <geofannyher@gmail.co>
2024-08-23 12:27:46 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,34 @@
 "use client";
 
-import { socket } from "@/lib/socket";
 import { supabase } from "@/lib/supabase";
 import React, { useEffect, useRef, useState } from "react";
+import { socket } from "@/lib/socket";
 
 const PlayVideo: React.FC = () => {
   const [videoIdle, setVideoIdle] = useState("");
   const [audioUrl, setAudioUrl] = useState("");
-  const [isPlaying, setIsPlaying] = useState(false);
-  const [timeStart, setTimeStart] = useState(0);
   const [idleTimeStart, setIdleTimeStart] = useState(0);
   const [idleTimeEnd, setIdleTimeEnd] = useState(0);
-  const [isPlayingWithoutAudio, setIsPlayingWithoutAudio] = useState(false);
   const audioRef = useRef<HTMLAudioElement>(null);
   const videoRef = useRef<HTMLVideoElement>(null);
-  const [miraIdle, setMiraIdle] = useState("");
-
+  const [timeStart, setTimeStart] = useState(0);
+  const [timeEnd, setTimeEnd] = useState(0);
+  const [modelStream, setModelStream] = useState("");
+  const [isAudioPlaying, setIsAudioPlaying] = useState(false);
+  const [isOnlyAudio, setIsOnlyAudio] = useState(false);
   const fetchDataModel = async () => {
     try {
       const { data, error } = await supabase.from("model").select("*");
       if (error) throw error;
 
       if (data && data.length > 0) {
-        const mira = data.find((item) => item.model_name === "kokovin");
+        const models = data.find((item) => item.model_name === "kokovin");
         const modelIdle = localStorage.getItem("modelstream");
 
-        const setIdleData = (model: any) => {
-          setIdleTimeStart(Number(model.time_start));
-          setIdleTimeEnd(Number(model.time_end));
-        };
-
-        if (mira && modelIdle === "kokovin") {
-          setMiraIdle(mira.video_url);
-          setIdleData(mira);
+        if (models && modelIdle === "kokovin") {
+          setModelStream(models.video_url);
+          setIdleTimeStart(Number(models.time_start));
+          setIdleTimeEnd(Number(models.time_end));
         }
       }
     } catch (error) {
@@ -46,97 +42,119 @@ const PlayVideo: React.FC = () => {
 
   useEffect(() => {
     const modelIdle = localStorage.getItem("modelstream");
-    if (modelIdle === "kokovin" && miraIdle) {
-      setVideoIdle(miraIdle);
-      setTimeStart(idleTimeStart);
-      setIdleTimeEnd(idleTimeEnd);
-    } else {
-      setVideoIdle("");
+    if (modelIdle === "kokovin" && modelStream) {
+      setVideoIdle(modelStream);
     }
-  }, [miraIdle, idleTimeStart, idleTimeEnd]);
+  }, [modelStream]);
 
-  useEffect(() => {
-    socket.on("receive_message", ({ audio_url, time_start, time_end }) => {
-      if (!audio_url && videoRef.current) {
-        console.log("Playing video from", time_start, "to", time_end);
-        videoRef.current.currentTime = time_start;
-        videoRef.current.muted = false; // Aktifkan audio
-        videoRef.current.play();
-        setIsPlayingWithoutAudio(true); // Set state untuk menandakan video tanpa audio sedang diputar
-      } else if (audio_url) {
-        setAudioUrl(audio_url);
-        setTimeStart(time_start);
-        setIdleTimeEnd(time_end);
-        setIsPlaying(true);
-        if (videoRef.current) {
-          videoRef.current.currentTime = time_start;
-          videoRef.current.muted = true; // Mute video saat audio diputar
-          videoRef.current.loop = false; // Nonaktifkan loop untuk mode khusus
-          videoRef.current.play().catch((error) => {
-            console.error("Error playing video:", error);
-          });
-          setIsPlayingWithoutAudio(false); // Reset state ketika audio diputar
-        }
-      }
-    });
-
-    return () => {
-      socket.off("receive_message");
-    };
-  }, [idleTimeStart]);
-
-  useEffect(() => {
-    if (audioUrl && audioRef.current) {
-      audioRef.current.src = audioUrl;
-      audioRef.current.load();
-      audioRef.current.volume = 0.2;
-      audioRef.current.play().catch((error) => {
-        console.error("Error playing audio:", error);
+  const playIdleVideo = () => {
+    if (videoRef.current && !isAudioPlaying) {
+      videoRef.current.currentTime = idleTimeStart;
+      videoRef.current.play().catch((error) => {
+        console.error("Error playing video:", error);
       });
     }
-  }, [audioUrl]);
-
-  const handleAudioEnded = () => {
-    console.log("Audio ended");
-    setIsPlaying(false);
-    if (videoRef.current) {
-      videoRef.current.currentTime = idleTimeStart;
-      videoRef.current.play(); // Memutar kembali video idle
-    }
-    socket.emit("audio_finished");
   };
 
+  useEffect(() => {
+    const handleReceiveMessage = ({
+      audio_url,
+      time_start,
+      time_end,
+    }: {
+      audio_url: string;
+      time_start: number;
+      time_end: number;
+    }) => {
+      if (audio_url) {
+        if (audio_url === "only") {
+          console.log("on");
+          setIsOnlyAudio(true);
+          setTimeStart(time_start);
+          setTimeEnd(time_end);
+          setAudioUrl(audio_url);
+        } else {
+          setIsOnlyAudio(false);
+          setTimeStart(time_start);
+          setTimeEnd(time_end);
+          setAudioUrl(audio_url);
+        }
+      }
+    };
+
+    socket.on("receive_message", handleReceiveMessage);
+    return () => {
+      socket.off("receive_message", handleReceiveMessage);
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log(isOnlyAudio, audioUrl, "cek");
+    if (audioUrl && audioUrl !== "only" && videoRef.current) {
+      setIsAudioPlaying(true);
+      videoRef.current.currentTime = timeStart; // Set the video start time
+      videoRef.current.play().catch((error) => {
+        console.error("Error playing video:", error);
+      });
+
+      if (audioRef.current) {
+        audioRef.current.src = audioUrl;
+        audioRef.current.load();
+        audioRef.current.play().catch((error) => {
+          console.error("Error playing audio:", error);
+        });
+      }
+    } else if (isOnlyAudio && audioUrl === "only") {
+      // Handle "only" case, start video without audio
+      console.log("play lah boi");
+      if (videoRef.current) {
+        videoRef.current.currentTime = timeStart;
+        videoRef.current.muted = false; // Unmute video
+        videoRef.current.play().catch((error) => {
+          console.error("Error playing video:", error);
+        });
+      }
+    } else {
+      setIsAudioPlaying(false);
+    }
+  }, [audioUrl]);
+  const handleAudioEnded = () => {
+    setAudioUrl("");
+    setIsAudioPlaying(false);
+    playIdleVideo();
+    socket.emit("audio_finished");
+  };
   const handleTimeUpdate = () => {
     if (videoRef.current) {
       if (
-        isPlayingWithoutAudio &&
-        videoRef.current.currentTime >= idleTimeEnd
+        audioUrl &&
+        audioUrl !== "only" &&
+        videoRef.current.currentTime >= timeEnd
       ) {
-        console.log(
-          "Finished playing video without audio, going back to idle."
-        );
-        videoRef.current.currentTime = idleTimeStart; // Reset ke waktu awal idle
-        videoRef.current.muted = true; // Matikan audio saat kembali ke idle
-        videoRef.current.play(); // Putar video idle
-        setIsPlayingWithoutAudio(false); // Reset state
+        videoRef.current.currentTime = timeStart;
+        videoRef.current.play().catch((error) => {
+          console.error("gagal play saat ada audio", error);
+        });
       } else if (
-        !isPlayingWithoutAudio &&
-        videoRef.current.currentTime >= timeStart &&
-        videoRef.current.currentTime >= idleTimeEnd
+        isOnlyAudio &&
+        audioUrl == "only" &&
+        videoRef.current.currentTime >= timeEnd
       ) {
         videoRef.current.currentTime = idleTimeStart;
-        videoRef.current.muted = true; // Matikan audio saat kembali ke idle
-        videoRef.current.play(); // Putar video idle
-      }
-    }
-  };
-
-  const handleLoadedMetadata = () => {
-    if (videoRef.current) {
-      videoRef.current.currentTime = timeStart;
-      if (isPlaying) {
+        videoRef.current.muted = true;
+        videoRef.current
+          .play()
+          .catch((error) => {
+            console.error("gagal play setelah hanya video", error);
+          })
+          .finally(() => {
+            setIsOnlyAudio(false);
+            setAudioUrl("");
+          });
+      } else if (!audioUrl && videoRef.current.currentTime >= idleTimeEnd) {
+        videoRef.current.currentTime = idleTimeStart;
         videoRef.current.play().catch((error) => {
-          console.error("Error playing video:", error);
+          console.error("gagal play saat video kembali ke idle", error);
         });
       }
     }
@@ -156,11 +174,10 @@ const PlayVideo: React.FC = () => {
               {videoIdle && (
                 <video
                   ref={videoRef}
+                  onTimeUpdate={handleTimeUpdate}
                   autoPlay
                   muted
                   controls
-                  onTimeUpdate={handleTimeUpdate}
-                  onLoadedMetadata={handleLoadedMetadata}
                   src={videoIdle}
                 >
                   <source src={videoIdle} type="video/mp4" />
@@ -171,7 +188,7 @@ const PlayVideo: React.FC = () => {
           </div>
         </div>
       </div>
-      {audioUrl && (
+      {audioUrl && audioUrl !== "only" && (
         <audio ref={audioRef} onEnded={handleAudioEnded} autoPlay controls>
           <source src={audioUrl} type="audio/mpeg" />
           Your browser does not support the audio element.
