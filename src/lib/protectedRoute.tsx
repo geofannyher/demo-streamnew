9aeac742d789211992e06f8c50f217bbbe28e696
geofannyher <geofannyher@gmail.com>
2024-10-15 12:43:04 +0700

    feat: add reset btn w auth page

--- src/lib/protectedRoute.tsx
+++ src/lib/protectedRoute.tsx
@@ -1,37 +1,37 @@
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
-import { supabase } from "./supabase";
-import { Session } from "@supabase/supabase-js";
-import Splash from "@/components/Splash";
-const requireAuth = <P extends object>(
-  WrappedComponent: React.ComponentType<P>
-) => {
-  return (props: P) => {
-    const [loading, setLoading] = useState(true);
-    const [session, setSession] = useState<Session | null>(null);
-    const router = useRouter();
+// import { useRouter } from "next/navigation";
+// import { useEffect, useState } from "react";
+// import { supabase } from "./supabase";
+// import { Session } from "@supabase/supabase-js";
+// import Splash from "@/components/Splash";
+// const requireAuth = <P extends object>(
+//   WrappedComponent: React.ComponentType<P>
+// ) => {
+//   return (props: P) => {
+//     const [loading, setLoading] = useState(true);
+//     const [session, setSession] = useState<Session | null>(null);
+//     const router = useRouter();
 
-    useEffect(() => {
-      const getSession = async () => {
-        const { data, error } = await supabase.auth.getSession();
-        if (error || !data.session) {
-          console.log(data);
-          router.push("/login");
-        } else {
-          setSession(data.session);
-          setLoading(false);
-        }
-      };
+//     useEffect(() => {
+//       const getSession = async () => {
+//         const { data, error } = await supabase.auth.getSession();
+//         if (error || !data.session) {
+//           console.log(data);
+//           router.push("/login");
+//         } else {
+//           setSession(data.session);
+//           setLoading(false);
+//         }
+//       };
 
-      getSession();
-    }, [router]);
+//       getSession();
+//     }, [router]);
 
-    if (loading) {
-      return <Splash />;
-    }
+//     if (loading) {
+//       return <Splash />;
+//     }
 
-    return <WrappedComponent session={session} {...props} />;
-  };
-};
+//     return <WrappedComponent session={session} {...props} />;
+//   };
+// };
 
-export default requireAuth;
+// export default requireAuth;
